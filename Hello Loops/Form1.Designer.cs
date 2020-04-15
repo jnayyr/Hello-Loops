00fa1a4c1b5bf6b2597753a2c3633ec7a6fb600a
Ajay Nair <AJ4942BO@go.minnstate.edu>
2020-04-15 15:16:10 -0500

    Final Commit.

--- Hello Loops/Form1.Designer.cs
+++ Hello Loops/Form1.Designer.cs
@@ -28,57 +28,60 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.btnFor = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnWhile
             // 
-            this.button1.Location = new System.Drawing.Point(73, 146);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnWhile.Location = new System.Drawing.Point(57, 122);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(191, 47);
+            this.btnWhile.TabIndex = 0;
+            this.btnWhile.Text = "While Loop";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
             // 
-            // button2
+            // btnDoWhile
             // 
-            this.button2.Location = new System.Drawing.Point(353, 196);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Location = new System.Drawing.Point(293, 122);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(199, 47);
+            this.btnDoWhile.TabIndex = 1;
+            this.btnDoWhile.Text = "Do While Loop";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
             // 
-            // button3
+            // btnFor
             // 
-            this.button3.Location = new System.Drawing.Point(613, 248);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnFor.Location = new System.Drawing.Point(543, 122);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(165, 47);
+            this.btnFor.TabIndex = 2;
+            this.btnFor.Text = "For Loop";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(752, 268);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnWhile);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Hello Loops";
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.Button btnFor;
     }
 }
 
--- Hello Loops/Form1.cs
+++ Hello Loops/Form1.cs
@@ -16,5 +16,34 @@ namespace Hello_Loops
         {
             InitializeComponent();
         }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            DialogResult selected = MessageBox.Show("Do you want to see some MessageBox dialogs?", "While", MessageBoxButtons.YesNo);
+
+            while (selected == DialogResult.Yes)
+            {
+                selected = MessageBox.Show("Here is a MessageBox. Do you want another?", "While", MessageBoxButtons.YesNo);
+            }
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            DialogResult selected;
+
+            do
+            {
+                selected = MessageBox.Show("Do you want another MessageBox", "Do While", 
+                    MessageBoxButtons.YesNo);
+            } while (selected == DialogResult.Yes);
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            for(int m = 0; m < 5; m++)
+            {
+                MessageBox.Show("Here's MessageBox " + m);
+            }
+        }
     }
 }
